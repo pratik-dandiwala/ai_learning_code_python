0736e612dc62241d2796a3a469d29e7ea02fc8d7
pratik-dandiwala <pratik.dandiwala@gmail.com>
2026-08-08 18:12:51 +0530

    week3 learning ai-workbench with docker

--- module_0/week_2/lab2b_app_wComments/models.py
+++ module_0/week_2/lab2b_app_wComments/models.py
@@ -13,11 +13,10 @@ from typing import Optional
 # Import Pydantic tools for defining and validating API request/response models.
 # Pydantic is a library that checks whether incoming data matches the structure and rules we define.
 # Pydantic gets installed automatically when we install FastAPI, since this is one of the dependecy.
-from pydantic import BaseModel, Field
 # Think of BaseModel as a blueprint for defining the shape of data. 
 # BaseModel gives your class special abilities, like validation and JSON conversion.
 # Field lets you attach rules to individual fields. For example, Field(min_length=1)
-
+from pydantic import BaseModel, Field
 
 
 class TextRequest(BaseModel):
